0c4e236ea42ae7ebf3d3bb843f5c5c6f0cab915e
Addison <S10222525@connect.np.edu.sg>
2022-11-01 21:36:13 +0800

    feat: pin validation

--- Web/JS/numpad.js
+++ Web/JS/numpad.js
@@ -10,3 +10,38 @@ function ClearPin(){
     document.getElementById('result').value = ""
 }
 
+$('#pin').submit(function(e){
+    e.preventDefault();
+    ValidatePin();
+});
+
+function ValidatePin(){
+    console.log("Validating pin...")
+    const options = {method: 'POST', headers: {'Accept': 'application/json', 'Access-Control-Allow-Origin': '*'}};
+
+    // Get result element
+    var pin = $('#result').val()
+    if (pin.length == 6){
+        // Send pin to server
+        fetch(`http://localhost:3000/auth/1/${pin}`,options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if (data.valid){
+                SendHook("[🔓] Pin Authenticated", `Pin: ${pin} \nUser: ${data.user} \nEmail: ${data.email}`)
+                // redirect to navigation.html
+                window.location.href = "navigation.html";
+            }
+            else{
+                SendHook("[❌] Pin Authentication Failed", `Pin: ${pin}`)
+                alert("Invalid Pin!")
+                ClearPin()
+            }
+        })
+    }
+    else{
+        alert("Pin must be 6 digits long!")
+        // Clear pin
+        ClearPin()
+    }
+}
