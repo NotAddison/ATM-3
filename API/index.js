d49f8d341da7a7739649b7e37b09bd4b73992598
Addison <S10222525@connect.np.edu.sg>
2022-11-13 13:34:37 +0800

    feat: dPins dictionary values reset

--- API/index.js
+++ API/index.js
@@ -72,6 +72,7 @@ app.get("/", (req, res, next)=>{
 // -------- [ GET variables ] --------
 app.get("/variables", (req, res, next)=>{
     res.status(200).json({
+        "dPins" : dPins,
         'gUser': gUser,
         'gPin': gPin,
         'gHash': gHash,
@@ -93,7 +94,11 @@ app.get("/reset", (req, res, next)=>{
     isCovered = false;
     isRequestingBio = false;
 
+    // Reset dPins
+    ResetDPins();
+
     res.status(200).json({
+        "dPins" : dPins,
         'gUser': gUser,
         'gPin': gPin,
         'gHash': gHash,
@@ -104,6 +109,12 @@ app.get("/reset", (req, res, next)=>{
     });
     console.log(">> Reset variables");
 });
+
+function ResetDPins(){
+    for (var key in dPins) {
+        dPins[key]["isPwnedDismissed"] = false;
+    }
+}
 
 
 // -------- [ Pin Authentication (1) ] --------
