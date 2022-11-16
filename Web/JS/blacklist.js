62001bac8f3dd97310391ad72f98f2941fadd974
Addison <S10222525@connect.np.edu.sg>
2022-11-16 22:40:43 +0800

    fix: Merge Conflict

--- Web/JS/blacklist.js
+++ Web/JS/blacklist.js
@@ -10,38 +10,14 @@ function CheckScore() {
         for (key in dict){
             if (dict[key]["accountNo"] === accountNo){
                 score = dict[key]["score"]
-                if (score < 20){
+                if (score <= 20){
                     return Blacklist()
                 }
-                else if (score < 60){
-                    SendHook("[⚠] Suspicious Transfer Detected", "A user is transferring money to a suspicious account.")
-                    $("body").prepend(`
-                    <div class="flex flex-wrap min-h-screen w-full content-center justify-center py-10 rounded-lg absolute z-40" id="BlacklistPopup">
-                        <div class="flex flex-wrap content-center justify-center rounded-lg bg-gray-50 shadow-md w-[28rem] border border-gray-200">
-                            <div class="p-5">
-                            <!-- Header Text -->
-                            <div class="flex flex-col">
-                                <div class="flex content-center justify-center mb-2">
-                                    <img class="w-10" src="https://img.icons8.com/ios/512/delete-user-male.png"/>
-                                </div>
-                                <p class="text-black text-2xl text-center">Suspicious Account Detected</p> 
-                            </div>
-                                <hr class="border-t-4 grey mt-2">
-                                <br>
-                                <div class="text-black h-32 mb-5 text-center">
-                                    <p>Please be careful.</p>
-                                    <p>Your are currently transferring/withdrawing money regarding a suspicious account</p>
-                                    <br>
-                                    <p>Are you sure you want to continue?</p>
-                                </div>
-                                <div class="flex flex-wrap content-center justify-between">
-                                <button class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-lg" onclick="DismissBlacklist()">Dismiss</button>
-                                <button class="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded-lg" onclick="ConfirmBlacklist()">Confirm</button>
-                            </div>
-                        </div>
-                    </div>
-                `   );
-                    console.log(">> [Blacklist]: SHOWING POPUP");
+                else if (score <= 60){
+                    return CheckSuspicious()
+                }
+                else{
+                    ShowToast("Transfer Successful", "green", "✔");
                 }
             }
         }
@@ -80,46 +56,34 @@ function CheckSuspicious() {
 }
 
 function Blacklist() {
-    const options = {method: 'GET', headers: {'Accept': 'application/json', 'Access-Control-Allow-Origin': '*'}};
-    fetch(`http://localhost:3000/blacklist/`, options)
-        .then(response => response.json())
-        .then(response => {     
-            blackList = response["sus"];
-            console.log(blackList);
-            if (blackList.length > 0) {
-                if (blackList.includes(parseInt($("#transferAcc").val())))
-                    {
-                    SendHook("[⚠] Blacklisted Transfer Detected", "A user is transferring money to a blacklisted account.")
-                    $("body").prepend(`
-                    <div class="flex flex-wrap min-h-screen w-full content-center justify-center py-10 rounded-lg absolute z-40" id="BlacklistPopup">
-                        <div class="flex flex-wrap content-center justify-center rounded-lg bg-gray-50 shadow-md w-[28rem] border border-gray-200">
-                            <div class="p-5">
-                            <!-- Header Text -->
-                            <div class="flex flex-col">
-                                <div class="flex content-center justify-center mb-2">
-                                    <img class="w-10" src="https://img.icons8.com/ios/512/delete-user-male.png"/>
-                                </div>
-                                <p class="text-black text-2xl text-center">Blacklisted Account Detected</p> 
-                            </div>
-                                <hr class="border-t-4 grey mt-2">
-                                <br>
-                                <div class="text-black h-32 mb-5 text-center">
-                                    <p>Please be careful.</p>
-                                    <p>Your are currently transferring/withdrawing money regarding a blacklist account</p>
-                                    <br>
-                                    <p>Transfer not allowed.</p>
-                                </div>
-                                <div class="flex flex-wrap content-center justify-center">
-                                <button class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-lg" onclick="DismissBlacklist()">Dismiss</button>
-                            </div>
-                        </div>
+    console.log(">> [Blacklist]: BLACKLISTED");
+    SendHook("[⚠] Blacklisted Transfer Detected", "A user is transferring money to a blacklisted account.")
+    $("body").prepend(`
+        <div class="flex flex-wrap min-h-screen w-full content-center justify-center py-10 rounded-lg absolute z-40" id="BlacklistPopup">
+            <div class="flex flex-wrap content-center justify-center rounded-lg bg-gray-50 shadow-md w-[28rem] border border-gray-200">
+                <div class="p-5">
+                <!-- Header Text -->
+                <div class="flex flex-col">
+                    <div class="flex content-center justify-center mb-2">
+                        <img class="w-10" src="https://img.icons8.com/ios/512/delete-user-male.png"/>
                     </div>
-                `);
-                    console.log(">> [Blacklist]: SHOWING POPUP");
-                }
-            }
-        })
-        .catch(err => console.error(err));
+                    <p class="text-black text-2xl text-center">Blacklisted Account Detected</p> 
+                </div>
+                    <hr class="border-t-4 grey mt-2">
+                    <br>
+                    <div class="text-black h-32 mb-5 text-center">
+                        <p>Please be careful.</p>
+                        <p>Your are currently transferring/withdrawing money regarding a blacklist account</p>
+                        <br>
+                        <p>Transfer not allowed.</p>
+                    </div>
+                    <div class="flex flex-wrap content-center justify-center">
+                    <button class="bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-lg" onclick="DismissBlacklist()">Dismiss</button>
+                </div>
+            </div>
+        </div>
+    `);
+    console.log(">> [Blacklist]: SHOWING POPUP");
 }
 
 function DismissBlacklist(){
@@ -131,6 +95,6 @@ function ConfirmBlacklist(){
     $("#BlacklistPopup").remove();
     console.log("confirm")
     // TODO: Show toast message confirming transfer
-    ShowToast("Transfer Successful", "green");
+    ShowToast("Transfer Successful", "green", "✔");
 }
 
