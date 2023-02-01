7cb3315ff707a5343e51ab956377991f15da5534
Addison <S10222525@connect.np.edu.sg>
2023-02-01 18:27:40 +0800

    chore: code cleanup - combined live-feed.js with dashboard.js

--- Web/JS/dashboard.js
+++ Web/JS/dashboard.js
@@ -43,11 +43,42 @@ function DisplayATMStatus(id, status){
     $(`#atm-${id}`).addClass(css);
 }
 
+isOnline = false
 async function GetATMFeed(){
-    console.log(">> Retrieving ATM Live Camera Feed...")
-    // HTTP Request to local API (Returns Base64Encoded String)
-    // Get ATM Video Feed
-    // Update every second
+    // Attempt socket connection
+    // const socket = new WebSocket('ws://localhost:8765');
+    // socket.onmessage = function (event) {
+    //     let blob = new Blob([event.data], {type: 'image/jpeg'});
+    //     let objectURL = URL.createObjectURL(blob);
+    //     document.getElementById('feed-img').src = objectURL;
+    // };
+
+    // Forces browser to download image by chaning the URL
+    url = "http://localhost:3000/cv/"
+    options = { method: 'GET', headers: { 'Content-Type': 'application/json' } }
+
+    fetch(url, options)
+    .then(response => response.json())
+    .then(response => {
+        isOnline = response["CVOnline"]
+    })
+
+    
+    if (isOnline) {
+        source = `../../../feed.jpg?random=`+new Date().getTime();
+        document.getElementById('feed-img').src = source;
+    }
+    else {
+        source = '../../../Assets/Images/Dashboard/loading2.gif'
+        currect_source_name = document.getElementById('feed-img').src.split('/').pop()
+        
+
+        if (currect_source_name != source.split('/').pop()){
+            document.getElementById('feed-img').src = source;
+        }
+        console.log(">> [Live Feed]: CV is offline");
+    }
+
 }
 
 function AtmButtonPressed(id){
@@ -73,7 +104,9 @@ $(document).ready(function(){
 
     if (pageName == "atm.html"){
         GetLogs();
-        GetATMFeed();
+
+        // Every 500 ms, refresh the feed
+        setInterval(GetATMFeed, 500);
     }
     
 });
