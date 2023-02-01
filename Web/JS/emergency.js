78abfa0da796d3a5495dedc00518c418a2b97957
realelongatedmusket <s10222819@connect.np.edu.sg>
2023-02-01 23:04:39 +0800

    feat: add logout user function to website

--- Web/JS/emergency.js
+++ Web/JS/emergency.js
@@ -1,17 +1,28 @@
 console.log(">> [Running emergency.js]");
 
 function CheckEmergency() { 
-    const options = {method: 'GET', headers: {'Accept': 'application/json', 'Access-Control-Allow-Origin': '*'}};
-    fetch(`http://localhost:3000/emergency/`, options)
+    var options = {method: 'GET', headers: {'Accept': 'application/json', 'Access-Control-Allow-Origin': '*'}};
+    res = fetch(`http://localhost:3000/emergency/`, options)
         .then(response => response.json())
         .then(response => {     
             isEmergency = response["valid"]
             console.log(`>> isEmergency: ${isEmergency}`);
-            if (isEmergency == true) {
-                window.location.href('index.html')
-                console.log(">> [Emergency]: User is redirected");
+            if (isEmergency) {
+
+                    ShowToast("You have been logged out by an OCBC Staff.")
+                    setTimeout(function(){}, 1000);
+
+                    // Switch back to false
+                    url = "http://localhost:3000/emergency/"
+                    options = { method: 'POST', headers: { 'Content-Type': 'application/json' } }
+                    fetch(url, options)
+                    window.location.href = 'auth.html'
+                    console.log(">> [Emergency]: User is redirected");
                 }
+                return isEmergency
             }
         )
         .catch(err => console.error(err));
 }
+
+setInterval(CheckEmergency, 1000);
