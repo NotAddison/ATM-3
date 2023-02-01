7e9a189ba5ad85444c24c016b45475a5fabfc85a
Addison <S10222525@connect.np.edu.sg>
2023-02-02 02:10:54 +0800

    fix: ATM IP Binding

--- Python/Webhook/Hook.py
+++ Python/Webhook/Hook.py
@@ -21,6 +21,16 @@ def GetName():
 webhook = DiscordWebhook(url= GetWebhookURL())
 
 
+def BindIP():
+    url = 'http://localhost:3000/ip/'
+    data = {
+        'IP': g.ip,
+        'Lat': g.lat,
+        'Long': g.lng
+    }
+    requests.post(url, json=data)
+    
+
 # Send GENERIC Discord Webhook
 def SendHostageHook(test = True):
     embed = DiscordEmbed(title='⚠ HOSTAGE Situation Detected ⚠', description='Potential hostage situation!', color=242424)
@@ -40,13 +50,3 @@ def SendHostageHook(test = True):
     print(f"isHostage Webhook: {response}")
 
 # SendHostageHook(False)
-
-def InitIP():
-    url = 'http://localhost:3000/ip/'
-    data = {
-        'IP': g.ip,
-        'Lat': g.lat,
-        'Long': g.lng
-    }
-    requests.post(url, json=data)
-InitIP()
