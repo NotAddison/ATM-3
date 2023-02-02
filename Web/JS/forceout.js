1b931363b8091492a71ba5066d5a43ed038dd2da
Addison <S10222525@connect.np.edu.sg>
2023-02-02 16:35:51 +0800

    fix: force logout - fixed naming convention - add missing script to client ATM pages

--- Web/JS/forceout.js
+++ Web/JS/forceout.js
@@ -1,22 +1,22 @@
-console.log(">> [Running emergency.js]");
+console.log(">> [Running forceout.js]");
 
 function ForceLogout() { 
     var options = {method: 'GET', headers: {'Accept': 'application/json', 'Access-Control-Allow-Origin': '*'}};
-    res = fetch(`http://localhost:3000/emergency/`, options)
+    res = fetch(`http://localhost:3000/force-logout/`, options)
         .then(response => response.json())
         .then(response => {
-            isEmergency = response["valid"]
-            if (isEmergency) {
+            isForcedOut = response["valid"]
+            if (isForcedOut) {
                     SendLog("Logged out user")
                     ShowToast("You have been logged out by an OCBC Staff.")
                     setTimeout(function(){}, 1000);
                     // Switch back to false
-                    url = "http://localhost:3000/emergency/"
+                    url = "http://localhost:3000/force-logout/"
                     options = { method: 'POST', headers: { 'Content-Type': 'application/json' } }
                     fetch(url, options)
                     window.location.href = 'auth.html'
                 }
-                return isEmergency
+                return isForcedOut
             }
         )
         .catch(err => console.error(err));
